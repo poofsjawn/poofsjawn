93c32a28db1a5f4bd457530bb4ecc898b9bc9893
morganbutton <morganbuttongithub.com>
2022-08-20 23:11:43 -0400

    setup environment

---
+++ stretchy poofs/main.js
@@ -0,0 +1,42 @@
+import './style.css'
+
+
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGL1Renderer({
+    canvas:document.querySelector('#bg'),
+})
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+camera.position.setZ(30);
+
+renderer.render( scene, camera);
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1);
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+const controls = new OrbitControls( camera, renderer.domElement );
+
+camera.position.z = 5;
+controls.update();
+
+//shows on canvas
+//loops like p5
+function animate() {
+	requestAnimationFrame( animate );
+   //cube.rotation.x += 0.01;
+    //cube.rotation.y += 0.01;
+    controls.update();
+
+	renderer.render( scene, camera );
+}
+animate();
